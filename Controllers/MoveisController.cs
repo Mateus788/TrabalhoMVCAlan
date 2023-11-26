a10bc883f33ae40b8528146f881805eb7b38a8f9
Mateus788 <mateuscorreia788@gmail.com>
2023-11-26 17:39:37 -0300

    first commit

---
+++ Controllers/MoveisController.cs
@@ -0,0 +1,26 @@
+﻿using ListaMVCMoveisAlan.Models.ModelsView;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaMVCMoveisAlan.Controllers
+{
+    public class MoveisController : Controller
+    {
+        public IActionResult ListaMoveis()
+        {
+            List<ItemModelView> itemModelViews = new List<ItemModelView>();
+            itemModelViews.Add(new ItemModelView("Porta", true));
+            itemModelViews.Add(new ItemModelView("Janela", true));
+            itemModelViews.Add(new ItemModelView("Cadeira", false));
+            itemModelViews.Add(new ItemModelView("Cama", false));
+            itemModelViews.Add(new ItemModelView("Sofá", true));
+            itemModelViews.Add(new ItemModelView("Mesa", false));
+            itemModelViews.Add(new ItemModelView("Poltrona", true));
+            itemModelViews.Add(new ItemModelView("Fogão", true));
+            itemModelViews.Add(new ItemModelView("Microondas", true));
+            itemModelViews.Add(new ItemModelView("Geladeira", false));
+            itemModelViews.Add(new ItemModelView("Ventilador", true));
+            itemModelViews.Add(new ItemModelView("Chaleira", false));
+            return View(itemModelViews);
+        }
+    }
+}
---
+++ Models/ModelsView/ItemModelView.cs
@@ -0,0 +1,21 @@
+﻿namespace ListaMVCMoveisAlan.Models.ModelsView
+{
+    public class ItemModelView
+    {
+        public string nomeMoveis { get; set; }
+
+        public bool checkMoveis { get; set; }
+
+        public ItemModelView(string nomeMoveis, bool checkMoveis)
+        {
+            this.nomeMoveis = nomeMoveis;
+            this.checkMoveis = checkMoveis;
+        }
+
+        public ItemModelView()
+        {
+
+
+        }
+    }
+}
